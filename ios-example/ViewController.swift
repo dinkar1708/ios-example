e140db9a45ac544ae7e88fbb07f7167889105138
Dinakar Maurya <dinakar.maurya@dena.jp>
2020-01-29 10:15:18 +0900

    Initial Commit

---
+++ ios-example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-example
+//
+//  Created by Dinakar Prasad Maurya on 2020/01/29.
+//  Copyright © 2020 Dinakar Prasad Maurya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
